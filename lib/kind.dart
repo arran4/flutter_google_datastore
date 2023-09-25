675af40158fbc454a6d6bcbac563ff9a3d841a53
Arran Ubels <arran4@gmail.com>
2023-09-25 19:09:08 +1000

    Use a cursor, and an infinite scroller

--- lib/kind.dart
+++ lib/kind.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_google_datastore/database.dart';
 import 'package:flutter_google_datastore/datastoremain.dart';
 import 'package:googleapis/datastore/v1.dart' as dsv1;
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 final datastoreRequiredScopes = [
   "https://www.googleapis.com/auth/datastore",
@@ -25,18 +26,23 @@ class _KindContentsPageState extends State<KindContentsPage> {
   int offset = 0;
   int limit = 100;
   int pages = -1;
-  late Future<List<EntityRow>> rows;
-
-  @override
-  void initState() {
-    rows = retrieveRows();
-  }
+  String? startCursor;
+  final PagingController<int, EntityRow> _pagingController = PagingController(
+      firstPageKey: 0);
 
   void closePressed() async {
     if (!Navigator.canPop(context)) {
       return;
     }
     Navigator.pop(context);
+  }
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
   }
 
   @override
@@ -56,21 +62,12 @@ class _KindContentsPageState extends State<KindContentsPage> {
           // ),
         ],
       ),
-      body: FutureBuilder(
-        future: rows,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            if (snapshot.error.runtimeType == dsv1.DetailedApiRequestError) {
-              return SelectableText('Authentication Error: ${snapshot.error}'); // TODO self provide details.
-            }
-            return SelectableText('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            final rows = snapshot.data;
-            return ListView.builder(
-              itemCount: rows!.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(rows[index].key),
+      body: PagedListView<int, EntityRow>(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<EntityRow>(
+            itemBuilder: (BuildContext context, EntityRow item, int index) =>
+                ListTile(
+                  title: Text(item.key),
                   trailing: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -80,13 +77,8 @@ class _KindContentsPageState extends State<KindContentsPage> {
                       ),
                     ],
                   ),
-                );
-              },
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
+                )
+        ),
       ),
     );
   }
@@ -94,19 +86,45 @@ class _KindContentsPageState extends State<KindContentsPage> {
   Future<List<EntityRow>> retrieveRows() async {
     List<EntityRow> results = [];
 
-    dsv1.RunQueryResponse response = await widget.dsApi!.projects.runQuery(dsv1.RunQueryRequest(
-      query: dsv1.Query(kind: [dsv1.KindExpression(name: widget.kind.name)], limit: limit, offset: offset),
-      partitionId: widget.kind.namespace != null ? dsv1.PartitionId(namespaceId: widget.kind.namespace!.name) : null,
-    ), widget.project.projectId);
-    results.addAll(response?.batch?.entityResults?.map((e) => e.entity)?.whereType<dsv1.Entity>()?.map((e) => EntityRow(entity: e)) ?? []);
+    dsv1.RunQueryResponse response = await widget.dsApi!.projects.runQuery(
+        dsv1.RunQueryRequest(
+          query: dsv1.Query(kind: [dsv1.KindExpression(name: widget.kind.name)],
+              startCursor: startCursor,
+              limit: limit,
+              offset: offset),
+          partitionId: widget.kind.namespace != null ? dsv1.PartitionId(
+              namespaceId: widget.kind.namespace!.name) : null,
+        ), widget.project.projectId);
+    startCursor = response?.batch?.endCursor;
+    results.addAll(
+        response?.batch?.entityResults?.map((e) => e.entity)?.whereType<
+            dsv1.Entity>()?.map((e) => EntityRow(entity: e)) ?? []);
 
     return results;
+  }
+
+  void _fetchPage(int pageKey) async {
+    try {
+      final newItems = await retrieveRows();
+      final isLastPage = newItems.length < limit;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
   }
 }
 
 class EntityRow {
   dsv1.Entity entity;
+
   EntityRow({required this.entity});
 
-  String get key => entity.key?.path?.map((e) => "${e.kind ?? ""} ( ${e.name ?? e.id ?? ""} )").join(" => ") ?? "#KEY ERROR";
+  String get key =>
+      entity.key?.path?.map((e) => "${e.kind ?? ""} ( ${e.name ?? e.id ??
+          ""} )").join(" => ") ?? "#KEY ERROR";
 }
