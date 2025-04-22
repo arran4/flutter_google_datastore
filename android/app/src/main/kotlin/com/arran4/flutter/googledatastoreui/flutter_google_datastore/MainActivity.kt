f4f062893b637e72b90f7d93f5108e6b7419762c
Arran Ubels <arran4@gmail.com>
2025-04-22 18:30:30 +1000

    Regenerated android

--- android/app/src/main/kotlin/com/arran4/flutter/googledatastoreui/flutter_google_datastore/MainActivity.kt
+++ android/app/src/main/kotlin/com/arran4/flutter/googledatastoreui/flutter_google_datastore/MainActivity.kt
@@ -2,5 +2,4 @@ package com.arran4.flutter.googledatastoreui.flutter_google_datastore
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
